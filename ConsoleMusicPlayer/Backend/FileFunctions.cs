54ea8cfc31ecde82a005a1122bdde05cc736f9b9
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-07-12 15:00:53 +0200

    Add files via upload

---
+++ ConsoleMusicPlayer/Backend/FileFunctions.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleMusicPlayer.Frontend;
+
+namespace ConsoleMusicPlayer.Backend
+{
+    internal class FileFunctions
+    {
+        private Messages _messages;
+
+        public FileFunctions(Messages messages)
+        {
+            _messages = messages;
+        }
+
+        public string LoadSong(string loadedSong = "")
+        {
+            if (loadedSong == "")
+            {
+                _messages.DisplayLoadManually();
+                string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                string songName = $"\\{Console.ReadLine()}.mp3";
+                string fullPath = musicFolder + songName;
+                FileInfo fileInfo = new FileInfo(fullPath);
+                bool checkPath = fileInfo.Exists;
+                if (checkPath)
+                {
+                    return fullPath;
+                }
+                else
+                {
+                    LoadSong();
+                }
+            }
+            return loadedSong;
+        }
+
+        public string[] GetMusicFromFolder()
+        {
+            string presentMusicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            string[] getSongsFromMusicFolder = Directory.GetFiles(presentMusicFolder, "*.mp3", SearchOption.TopDirectoryOnly);
+            return getSongsFromMusicFolder;
+        }
+    }
+}
---
+++ ConsoleMusicPlayer/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleMusicPlayer;
+using ConsoleMusicPlayer.Backend;
+using ConsoleMusicPlayer.Frontend;
+using WMPLib;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+SetColorCombo colorCombo = new SetColorCombo();
+AsciiArt asciiArt = new AsciiArt();
+Positions position = new Positions();
+WindowsMediaPlayer player = new WindowsMediaPlayer();
+EasterEgg visualizer = new EasterEgg();
+Messages messages = new Messages(colorCombo, asciiArt, position, visualizer);
+FileFunctions fileFunctions = new FileFunctions(messages);
+ConsoleMediaPlayer consoleMediaPlayer = new ConsoleMediaPlayer(player, messages, fileFunctions);
+
+int choice = 0;
+bool choiceTest=false;
+messages.RenderTitle();
+messages.RenderAsciiMenu();
+consoleMediaPlayer.PrepareSongList(fileFunctions.GetMusicFromFolder());
+
+while (choice != 9)
+{
+    messages.DisplayMenuChoice();
+    choiceTest = int.TryParse(Console.ReadLine(), out choice);
+    if (choiceTest)
+    {
+        messages.ClearNoFileLoadedMessage();
+        consoleMediaPlayer.ChoicePatchTrough(choice);
+    }
+}
