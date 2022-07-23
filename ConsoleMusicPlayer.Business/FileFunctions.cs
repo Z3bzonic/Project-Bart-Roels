6080984a9b88be0b6107206ca79ce3d69c9c2e0e
Bart Roels <roelsbart@gmail.com>
2022-07-24 01:23:39 +0200

    multi project
    (cherry picked from commit 045d9a73167844aeac704014a132d1798f66290f)

---
+++ ConsoleMusicPlayer.Business/FileFunctions.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleMusicPlayer.Business
+{
+    public class FileFunctions
+    {
+        public FileFunctions()
+        {
+        }
+
+        public string GetFullFilePath(string fileName)
+        {
+            string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            string songName = $"\\{fileName}.mp3";
+            string fullPath = musicFolder + songName;
+            return fullPath;
+        }
+
+        public bool FileExists(string fullPath)
+        {
+            return File.Exists(fullPath);
+        }
+
+        //public string LoadSong(string loadedSong = "")
+        //{
+        //    if (loadedSong == "")
+        //    {
+        //        _messages.DisplayLoadManually();
+        //        string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        //        string songName = $"\\{Console.ReadLine()}.mp3";
+        //        string fullPath = musicFolder + songName;
+        //        fullPath = File.Exists(fullPath) ? fullPath : LoadSong();
+        //    }
+        //    return loadedSong;
+        //}
+
+        public string[] GetMusicFromFolder()
+        {
+            string presentMusicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            string[] getSongsFromMusicFolder = Directory.GetFiles(presentMusicFolder, "*.mp3", SearchOption.TopDirectoryOnly);
+            return getSongsFromMusicFolder;
+        }
+    }
+}
---
+++ ConsoleMusicPlayer.Business/StatesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMusicPlayer.Business
+{
+    public class StatesDTO
+    {
+        public bool toggleStatePlayer { get; set; } = false;
+        public bool toggleMutePlayer { get; set; } = false;
+        public bool pathPresent { get; set; } = false;
+        public int volume { get; set; } = 50;
+        public string getSong { get; set; } = "";
+        public int songListCycle { get; set; } = 0;
+
+    }
+}
